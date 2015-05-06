bcc98d59df765aa820cefe9244bf418ef316d5cc
MK <mk10@hotmail.de>
2015-05-06 22:25:55 +0200

    Hast du eine Berufsausbildung?

--- Studienberatung.java
+++ Studienberatung.java
@@ -9,10 +9,21 @@ public class Studienberatung{
 	
 	String answer = scanner.next();
 	
+<<<<<<< HEAD
 	System.out.println("Hast du Fachabitur?");
 	
 	answer = scanner.next();
 	
 	if (answer.equalsIgnoreCase("n")){}
+=======
+	System.out.println("Bist du wissbegierig?");
+	
+	answer = scanner.next();
+>>>>>>> bist-du-wissbegierig
+
+	System.out.println("Hast du eine Berufsausbildung?");
+	
+	anwer = scanner.next();
+
 	}
 }
